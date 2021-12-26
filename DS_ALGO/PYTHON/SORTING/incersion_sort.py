39a1773b9fbcf1b5e80d7045a17bdc08d2bf88c5
Maruf Hasan <maruf3684@gmail.com>
2021-12-26 15:45:46 +0600

    sorting

---
+++ DS_ALGO/PYTHON/SORTING/incersion_sort.py
@@ -0,0 +1,12 @@
+
+arr = [8,4,1,5,9,2]
+
+for i in range(1,arr.__len__(),1):
+    temp=arr[i]
+    j=i-1
+    while((j>=0) and (temp<arr[j])):
+        arr[j+1]=arr[j]
+        j-=1
+    arr[j+1]=temp
+        
+print(arr)
---
+++ DS_ALGO/PYTHON/SORTING/selection_sort_reverse.py
@@ -0,0 +1,13 @@
+arr=[4,1,9,2,3,6]
+
+for i in range(arr.__len__()-1):
+    min=i
+    for j in range(i,arr.__len__(),1):
+        if(arr[j]>arr[min]):
+            min=j
+    if (arr[min]!=arr[i]):
+        arr[i], arr[min]=arr[min], arr[i]     
+        
+
+print(arr)
+        
